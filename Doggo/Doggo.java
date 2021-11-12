0de247b9c660870f7d4927ff422f4bf903aaf9dc
Kristian Moltke Reitzel <k.reitzel@outlook.dk>
2021-11-12 11:00:13 +0100

    Exercise Doggo1 complete

---
+++ Doggo/Doggo.java
@@ -0,0 +1,22 @@
+package Doggo;
+
+/**
+ * @author Kristian Moltke Reitzel
+ * Doggo
+ */
+public class Doggo {
+    private String name;
+
+    public Doggo(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void makeBark() {
+        System.out.println(name + " said: Woof woof");
+    }
+    
+}

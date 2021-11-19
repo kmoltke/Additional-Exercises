56fa059c7bdcfabb8fb6affe16b35e2aa8584d90
Kristian Moltke Reitzel <k.reitzel@outlook.dk>
2021-11-19 09:01:19 +0100

    Add exercise description

--- SimpleScanner/Main.java
+++ SimpleScanner/Main.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
 
 /**
- * Main
+ * This exercise will train your skill in using a scanner. The task is simple: Get the sum of two integers. All test cases contain an integer x and an integer y.
+
+Your task is to scan the input, calculate, and then print the sum.
+
+Hint: Remember to import the Scanner
  */
 public class Main {
     public int sum(int x, int y) {

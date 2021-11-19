c9ab2fb1f9c765c6b7f43235a0c3de006a7c42c5
Kristian Moltke Reitzel <k.reitzel@outlook.dk>
2021-11-19 08:05:30 +0100

    Start up

---
+++ SimpleScanner/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextInt()) {
+            
+        }
+        in.close();
+    }
+}

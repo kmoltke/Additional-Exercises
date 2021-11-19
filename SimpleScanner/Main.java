e8c80d5bf61bcfc930e6fbdb2dcea3ebd4a01fb0
Kristian Moltke Reitzel <k.reitzel@outlook.dk>
2021-11-19 08:59:36 +0100

    test pass

--- SimpleScanner/Main.java
+++ SimpleScanner/Main.java
@@ -4,12 +4,19 @@ import java.util.Scanner;
  * Main
  */
 public class Main {
+    public int sum(int x, int y) {
+        return x + y;
+    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         while (in.hasNextInt()) {
-            
+            Main m = new Main();
+            int x = in.nextInt();
+            int y = in.nextInt();
+            System.out.println(m.sum(x, y));
         }
         in.close();
+
     }
 }

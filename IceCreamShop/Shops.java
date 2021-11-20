36a6072799b14f63790d2657479ec06554fd0cbd
Kristian Moltke Reitzel <k.reitzel@outlook.dk>
2021-11-20 20:51:25 +0100

    8. add shopsWithFlavour()

--- IceCreamShop/Shops.java
+++ IceCreamShop/Shops.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.stream.*;
 
 public class Shops {
     private ArrayList<IceCreamShop> shops;
@@ -21,5 +22,13 @@ public class Shops {
 
     public int amountOfShops() {
         return shops.size();
+    }
+
+    public ArrayList<IceCreamShop> shopsWithFlavour(String flavour) {
+        ArrayList<IceCreamShop> shops;
+        shops = this.shops.stream()
+                .filter(s -> s.getVariants().contains(flavour))
+                .collect(Collectors.toCollection(ArrayList::new));
+        return shops;
     }
 }

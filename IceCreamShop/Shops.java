ed18b5d967e463ea8cb2363866a9ae0f0f2b60d8
Kristian Moltke Reitzel <k.reitzel@outlook.dk>
2021-11-20 20:20:26 +0100

    4. add getIcecreamShops()

--- IceCreamShop/Shops.java
+++ IceCreamShop/Shops.java
@@ -6,4 +6,10 @@ public class Shops {
     public Shops() {
         shops = new ArrayList<>();
     }
+
+    public ArrayList<IceCreamShop> getIceCreamShops() {
+        return shops;
+    }
+
+    
 }
